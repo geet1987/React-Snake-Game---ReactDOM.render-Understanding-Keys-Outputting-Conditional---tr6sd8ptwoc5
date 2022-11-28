62413f7dcf23367dc66dac9bc6bf57033e320a48
geet1987 <52599381+geet1987@users.noreply.github.com>
2022-11-28 10:09:29 +0530

    updated

--- src/components/Food.js
+++ src/components/Food.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 export default (props) => {
+import React from 'react';
 
+export default (props) => {
+
+  const style = {
+    left: `${props.dot[0]}%`,
+    top: `${props.dot[1]}%`
+  }
+
+  return (
+    <div className="snake-food" style={style}></div>
+  )
+}
  
 
   return (
